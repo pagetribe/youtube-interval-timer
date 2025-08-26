916d82d3536ef27729fcdbcf89204d94341f72af
PageTribe <pagetribemail@gmail.com>
2025-08-26 21:58:00 +1000

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // <-- THIS IS THE CRUCIAL LINE
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
